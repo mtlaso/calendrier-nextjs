4a055a43206bf8a643e2d307ced5c239452f1741
Danny D <danny@pop-os.localdomain>
2022-10-13 18:21:24 -0400

    Desactiver les boutons du header si l'utilisateur n'est pas connecté.

--- apps/frontend/components/calendar-header/CalendarHeader.tsx
+++ apps/frontend/components/calendar-header/CalendarHeader.tsx
@@ -1,14 +1,35 @@
 import React from "react";
 import Link from "next/link";
 
+import { TypeCalendarSyncStatus } from "../../types/TypeCalendarSyncStatus";
+
 import styles from "./CalendarHeader.module.sass";
 
 const CalendarHeader = (props: {
+  /**
+   * Le texte contentant le mois et l'année actuels
+   */
   headerText: string;
+  /**
+   * Aller au mois précédent
+   */
   clickBack: () => void;
+  /**
+   * Aller au prochain mois
+   */
   clickNext: () => void;
+  /**
+   * Afficher la modale d'informations
+   */
   showInfoModal: () => void;
+  /**
+   * Bouton pour aller à la date du jour
+   */
   goToCurrentMonth: () => void;
+  /**
+   * Statut de synchronisation du calendrier
+   */
+  syncStatus: TypeCalendarSyncStatus;
 }) => {
   return (
     <header className={styles.header}>
@@ -18,6 +39,8 @@ const CalendarHeader = (props: {
       <div>
         {/* bouton back */}
         <button
+          disabled={props.syncStatus !== "synced"}
+          style={{ cursor: props.syncStatus !== "synced" ? "not-allowed" : "pointer" }}
           onClick={() => {
             props.clickBack();
           }}>
@@ -26,6 +49,8 @@ const CalendarHeader = (props: {
 
         {/* bouton next */}
         <button
+          disabled={props.syncStatus !== "synced"}
+          style={{ cursor: props.syncStatus !== "synced" ? "not-allowed" : "pointer" }}
           onClick={() => {
             props.clickNext();
           }}>
